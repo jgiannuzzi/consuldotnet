9add66438f3e781d21756dbd8c07001b559e367b
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2020-12-11 17:43:00 +0000

    Test CI matrix

--- matrix.py
+++ matrix.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import json
+import os
+
 import pandas as pd
 import yaml
 
@@ -10,25 +12,27 @@ def load_matrix(data):
         index=pd.MultiIndex.from_product(
             data.values(),
             names=data.keys()
-            )
-        ).reset_index()
+        )
+    ).reset_index()
 
 
 def main():
-    doc = yaml.safe_load(open('matrix.yml'))
-    
-    matrix = load_matrix(doc['matrix'])
-    
-    for op in doc['operations']:
+    config = yaml.safe_load(os.environ.get('INPUT_CONFIG')
+
+    matrix = load_matrix(config['matrix'])
+
+    for op in config['operations']:
         op_matrix = load_matrix(op['matrix'])
         if op['type'] == 'merge':
             matrix = matrix.merge(op_matrix, on=op['match'], how='outer')
         elif op['type'] == 'add':
-            intersection = matrix.columns.intersection(op_matrix.columns).difference(op['match'])
-            matrix = matrix.append(matrix.drop(intersection, axis=1).merge(op_matrix, on=op['match'])).drop_duplicates()
+            intersection = matrix.columns.intersection(
+                op_matrix.columns).difference(op['match'])
+            matrix = matrix.append(matrix.drop(intersection, axis=1).merge(
+                op_matrix, on=op['match'])).drop_duplicates()
         else:
             print(f'Unknown operation type {op["type"]}')
-   
+
     matrix = matrix.sort_values(by=list(matrix.columns), ignore_index=True)
     include = matrix.to_dict(orient='records')
     for x in include:
@@ -36,6 +40,7 @@ def main():
             if pd.isnull(v):
                 del x[k]
     print('::set-output name=matrix::' + json.dumps({'include': include}))
+
 
 if __name__ == '__main__':
     main()
