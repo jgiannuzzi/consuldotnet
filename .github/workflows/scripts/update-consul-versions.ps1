3aeb1693f744a04c466d64f093c7bca432630f3d
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2022-04-21 19:06:27 +0200

    wip

--- .github/workflows/scripts/update-consul-versions.ps1
+++ .github/workflows/scripts/update-consul-versions.ps1
@@ -2,6 +2,17 @@ $workflow = ".github/workflows/ci.yml"
 $owner = "hashicorp"
 $repo = "consul"
 
+# Suppress telemetry reminder
+Set-GitHubConfiguration -SuppressTelemetryReminder
+
+# Use GITHUB_TOKEN if provided
+if ($Env:GITHUB_TOKEN -ne $null)
+{
+  $token = ($Env:GITHUB_TOKEN | ConvertTo-SecureString -AsPlainText -Force)
+  $cred = New-Object System.Management.Automation.PSCredential "username is ignored", $token
+  Set-GitHubAuthentication -Credential $cred -SessionOnly
+}
+
 # Fetch all the latest stable Consul releases
 $releases = Get-GitHubRelease -OwnerName $owner -RepositoryName $repo | Where-Object { ! $_.PreRelease }
 
